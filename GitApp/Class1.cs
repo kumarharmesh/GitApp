b0962b868606dbaff16282eeb0492e229a660e6e
Harmesh <kumarharmesh@gmail.com>
2020-03-09 14:17:16 +0530

    Add project files.

---
+++ GitApp/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitApp
+{
+    class DateClass
+    {
+        public static string GetDate()
+        {
+            return DateTime.Now.ToString();
+        }
+        public static string GetFormattedDate(string format)
+        {
+            return DateTime.Now.ToString(format);
+
+        }
+        public static double RandomNumber()
+        {
+            var r = new Random();
+            return r.NextDouble();
+            //>>>>>>> Stashed changes
+        }
+        public string Number()
+        {
+            return "Hello world";
+        }
+        public static double Root(int n)
+        {
+           double x= Math.Sqrt(n);
+            return x;
+        }
+    }
+}
---
+++ GitApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Action<string> printf = Console.WriteLine;
+            printf("Project Created");
+            printf(DateClass.GetDate());
+            Console.WriteLine("Project Created");
+            Console.WriteLine(DateClass.GetDate());
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+        public static bool Compare(int x, int y) => x > y;
+    }
+    class Emp
+    {
+        public string Name{get;set;}   
+        public byte Age{get;set;}   
+    }
+}
